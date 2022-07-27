432aeda737f858faae6ed2420243e8344185fca4
Muzaffar <mikayilov809@gmail.com>
2022-07-28 00:59:10 +0400

    came back

---
+++ month-2 (JavaScript Basics)/task17/17.1(area_of_rectangle).js
@@ -0,0 +1,2 @@
+// area_of_rectangle
+// User will enter the sides of rectangle: width and length. Create variables and assign these to variables. Later, calculate the area of the rectangle and alert it 
---
+++ month-2 (JavaScript Basics)/task17/17.2(age_calculator).js
@@ -0,0 +1,2 @@
+// age_calculator
+// User will enter his/her birth date. Find his/her age and alert it. 
