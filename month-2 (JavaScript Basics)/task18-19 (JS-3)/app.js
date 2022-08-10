7be094638cc763fe9f66292f2b17f365df7c61a9
Muzaffar <mikayilov809@gmail.com>
2022-08-10 17:31:02 +0400

    js-3 done

---
+++ month-2 (JavaScript Basics)/task18-19 (JS-3)/app.js
@@ -0,0 +1,93 @@
+// Exercise (1)
+
+// let numberOne = prompt('Enter the first number: ')
+// let numberTwo = prompt('Enter the second number: ')
+// alert(`The sum of the two entered number is: ${Number(numberOne)+Number(numberTwo)}.`)
+
+// Exercise (2)
+
+// let a = 1;
+// let c = ++a;
+// let d = c++;
+// alert(a); // 2
+// alert(c); // 3
+// alert(d++); // 2
+
+// Exercise (3)
+
+// alert(5 > 4); // True
+// alert("apple" > "pineapple"); // False
+// alert("2" > "12"); // True
+// alert(undefined == null); // True
+// alert(undefined === null); // False
+// alert(null == " 0 "); // False
+// alert(null === +"0"); // Fasle
+
+// Exercise (4)
+
+// let login = prompt('Enter the username: ')
+// if(login=='admin'){
+//     let password = prompt('Enter the password: ')
+//     if(password=='TheMaster'){
+//         alert('Welcome')
+//     }else if(password=='' || login==null){
+//         alert('Canceled')
+//     }else{
+//         alert('Wrong Password!')
+//     }
+// }else if(login=='' || login == null){
+//     alert('Canceled')
+// }else{
+//     alert('I don\'t know you')
+// }
+
+
+// Exercise (6)
+
+
+// let year = Number(prompt('Enter the year: '))
+// if(year%4==0 && year%100!=0 || year%400==0){
+//     alert('The year is leap')
+// } else {
+//     alert('the year is not leap')
+// }
+
+// Exercise (7)
+
+// if (browser=='Edge'){
+//     alert("You've got the Edge!")
+// }else if(browser =='Chrome'){
+//     alert("You've got the Chrome!")
+// }else if(browser =='Firefox'){
+//     alert("You've got the Firefox!")
+// }else if(browser =='Safari'){
+//     alert("You've got the Safari!")
+// }else if(browser =='Opera'){
+//     alert("Okay we support these browsers too!")
+// }else{
+//     alert('We hope that this page looks ok!')
+// }
+
+// Exercise (8)
+
+// let numberOne = Number(prompt('Enter the first number: '))
+// let numberTwo = Number(prompt('Enter the second number: '))
+// let operation = prompt('Choose the operation: + - * / ')
+
+// switch(operation){
+//     case '+':
+//         alert(numberOne+numberTwo)
+//         break
+//     case '-':
+//         alert(numberOne-numberTwo)
+//         break
+//     case '*':
+//         alert(numberOne*numberTwo)
+//         break
+//     case '/':
+//         alert(numberOne/numberTwo)
+//         break
+//     default:
+//         alert('No such case!')
+// }
+
