bb45c986e7c1a5fc1c8b84a125bd84f57e7d749b
Muzaffar <mikayilov809@gmail.com>
2022-08-10 15:29:46 +0400

    none

--- month-2 (JavaScript Basics)/task17/js/17.1(area_of_rectangle).js
+++
@@ -1,9 +0,0 @@
-// area_of_rectangle
-// User will enter the sides of rectangle: width and length. Create variables and assign these to variables. Later, calculate the area of the rectangle and alert it 
-
-
-let width = prompt('Enter the rectangle width');
-let length = prompt('Enter the rectangle lenght');
-let area_of_rectangle = width * length;
-console.log('Area of rectangle = ', area_of_rectangle);
-alert('Area_of_rectangle = ' + area_of_rectangle)
--- month-2 (JavaScript Basics)/task17/js/17.2(age_calculator).js
+++
@@ -1,8 +0,0 @@
-// age_calculator
-// User will enter his/her birth date. Find his/her age and alert it. 
-
-
-let birth_date = prompt('Enter the birth date as yy');
-let age = 2022 - birth_date;
-alert('your age is:'+ ' ' + age);
-console.log(age);
