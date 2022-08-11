96dad701a12fa8e201d581102e19f360549fcfa4
Muzaffar <mikayilov809@gmail.com>
2022-08-11 20:29:37 +0400

    js-7 little-changes

--- month-2 (JavaScript Basics)/task23 (JS-7)/app.js
+++ month-2 (JavaScript Basics)/task23 (JS-7)/app.js
@@ -53,3 +53,55 @@
 // }
 
 // objectProperyCount(User3)
+
+// -----JAVASCRIPT-7 OPTINAL TASKS------
+
+// -----Exercise (1)-----
+
+// let obj1 = {}
+// obj1.name = 'Laptop'
+// obj1.price = 1200
+// obj1.price = 1000
+// console.log(obj1.name,obj1.price)
+// delete obj1.name
+// console.log(obj1.name) // undefined
+
+// -----Exercise (2)-----
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Peter: 130
+// }
+
+// let sum = 0 
+// for(let i in salaries){
+//     sum+=salaries[i]
+        
+// }
+// console.log(sum)
+
+
+// -----Exercise (3)-----
+
+// let calculator = {
+//     read: function(){
+//         let value1=prompt('')
+//         return value1
+        
+//     },
+//     read2: function(){
+//         let value2 = prompt('')
+//         return value2
+//     },
+//     sum: function(a,b){
+//         return a+b
+//     },
+//     mull: function(a,b){
+//         return a*b
+//     }
+// }
+// // calculator.read()
+// // calculator.read2()
+// alert(calculator.sum(calculator.read(),calculator.read2()));
+// alert(calculator.mull(calculator.read(),calculator.read2()));
