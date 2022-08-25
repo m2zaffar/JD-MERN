52d843b27ec1f6d058c0c4068fb0f28662ceb5e1
Muzaffar <mikayilov809@gmail.com>
2022-08-25 20:05:31 +0400

    dom task done

---
+++ month-2 (JavaScript Basics)/task27 (JS-10)/app.js
@@ -0,0 +1,20 @@
+let grey = document.getElementById("grey");
+let red = document.getElementById("red");
+let blue = document.getElementById("blue");
+let yellow = document.getElementById("yellow");
+
+grey.onclick = function () {
+  document.body.style.backgroundColor = "grey";
+};
+
+red.onclick = function () {
+  document.body.style.backgroundColor = "red";
+};
+
+blue.onclick = function () {
+  document.body.style.backgroundColor = "blue";
+};
+
+yellow.onclick = function () {
+  document.body.style.backgroundColor = "yellow";
+};
